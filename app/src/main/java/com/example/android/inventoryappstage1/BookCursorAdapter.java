347c2d6a33b6e016a8d9788a282321aae27a2036
paintedlbird7 <rosaperezlarios@gmail.com>
2018-09-03 13:24:54 -0700

    book app inventory pt2 in progress working

--- app/src/main/java/com/example/android/inventoryappstage1/BookCursorAdapter.java
+++ app/src/main/java/com/example/android/inventoryappstage1/BookCursorAdapter.java
@@ -2,7 +2,6 @@ package com.example.android.inventoryappstage1;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -57,38 +56,65 @@ public class BookCursorAdapter extends CursorAdapter {
     public void bindView(View view, Context context, Cursor cursor) {
         // Find individual views that we want to modify in the list item layout
         TextView nameTextView = (TextView) view.findViewById(R.id.name);
-//        TextView priceTextView = (TextView) view.findViewById(R.id.price);
-//        TextView quantityTextView = (TextView) view.findViewById(R.id.quantity);
-        //TextView supplierTextView = (TextView) view.findViewById(R.id.supplier);
+        //TextView priceTextView = (TextView) view.findViewById(R.id.price);
+        //TextView quantityTextView = (TextView) view.findViewById(R.id.quantity);
+        TextView supplierTextView = (TextView) view.findViewById(R.id.supplier);
         //TextView phoneTextView = (TextView) view.findViewById(R.id.phone);
         TextView summaryTextView = (TextView) view.findViewById(R.id.summary);
 
         // Find the columns of book attributes that we're interested in
         int nameColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_BOOK_NAME);
-//        int priceColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_BOOK_PRICE);
-        //int quantityColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_BOOK_QUANTITY);
-//        int supplierColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_BOOK_SUPPLIER);
-//        int phoneColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_BOOK_PHONE);
+        int priceColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_BOOK_PRICE);
+        int quantityColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_BOOK_QUANTITY);
+        int supplierColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_BOOK_SUPPLIER);
+        int phoneColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_BOOK_PHONE);
 
 
         // Read the book attributes from the Cursor for the current book
         String bookName = cursor.getString(nameColumnIndex);
-//        String bookPrice = cursor.getString(priceColumnIndex);
-//        String bookQuantity = cursor.getString(quantityColumnIndex);
-//        String bookSupplier = cursor.getString(supplierColumnIndex);
-//        String bookPhone = cursor.getString(phoneColumnIndex);
+        String bookPrice = cursor.getString(priceColumnIndex);
+        String bookQuantity = cursor.getString(quantityColumnIndex);
+        //String bookSupplier = cursor.getString(supplierColumnIndex);
+        String bookPhone = cursor.getString(phoneColumnIndex);
 
         // If the book name is empty string or null, then use some default text
         // that says "Unknown name", so the TextView isn't blank.
-        if (TextUtils.isEmpty(bookName)) {
-            bookName = context.getString(R.string.unknown_book);
+//        if (TextUtils.isEmpty(bookName)) {
+//            bookName = context.getString(R.string.unknown_book);
 
             // Update the TextViews with the attributes for the current book
             nameTextView.setText(bookName);
-            //priceTextView.setText(bookPrice);
-            //quantityTextView.setText(bookQuantity);
-            //summaryTextView.setText(book);
+//            priceTextView.setText(bookPrice);
+//            quantityTextView.setText(bookQuantity);
+            //summaryTextView.setText(bookSummary);
+            //supplierTextView.setText(bookSupplier);
             //phoneTextView.setText(bookPhone);
         }
     }
-}
+
+
+
+//    public void bindView(View view, Context context, Cursor cursor) {
+//        TextView nameTextView = (TextView) view.findViewById(R.id.name);
+//                TextView summaryTextView = (TextView) view.findViewById(R.id.summary);
+//
+//        // Find the columns of book attributes that we're interested in
+//        int nameColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_BOOK_NAME);
+//        int priceColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_BOOK_PRICE);
+//
+//        // Read the book attributes from the Cursor for the current book
+//        String bookName = cursor.getString(nameColumnIndex);
+//        String bookPrice = cursor.getString(priceColumnIndex);
+//
+//        // If the pet breed is empty string or null, then use some default text
+//        // that says "Unknown breed", so the TextView isn't blank.
+//        if (TextUtils.isEmpty(bookPrice)) {
+//            bookPrice = context.getString(R.string.unknown_price);
+//
+//            // Update the TextViews with the attributes for the current pet
+//        nameTextView.setText(bookName);
+//        summaryTextView.setText(bookPrice);
+//  }
+//        }
+//
+//    }
